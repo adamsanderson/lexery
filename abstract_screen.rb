4fcee1a5c68b75aaf61e78289d648894168471db
Adam Sanderson <netghost@gmail.com>
2008-12-26 17:39:08 -0800

    adding infrastructure for managed game objects (should have done this a while ago).

--- abstract_screen.rb
+++ abstract_screen.rb
@@ -4,17 +4,35 @@ class AbstractScreen
   def initialize
     @window = Game.window
     @@cursor ||= Game.load_image("cursor.png", false)
+
+    @drawers = []
+    @updaters = []
+    @listeners = []
+  end
+  
+  def add(managed_object)
+    @drawers    << managed_object if managed_object.responds_to?(:draw)
+    @updaters   << managed_object if managed_object.responds_to?(:update)
+    @listeners  << managed_object if managed_object.responds_to?(:button_down)
+  end
+  
+  def remove(managed_object)
+    [@drawers, @updaters, @listeners].each{|list| list.delete(managed_object) }
   end
   
   def update
+    @updaters.each{|o| o.update}
   end
   
   def draw
     @@cursor.draw(window.mouse_x, window.mouse_y, Layers::CURSOR) if cursor_visible?
     window.draw_quad(0, 0, 0xffffffff, width, 0, 0xffffffff, 0, height, 0xFFF7D9FF, width, height, 0xFFF7D9FF)
+    
+    @drawers.each{|o| o.draw}
   end
   
   def button_down(id)
+    @listeners.each{|o| o.button_down(id)}
   end
   
   def button_up(id)
