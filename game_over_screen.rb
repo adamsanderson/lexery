b8e3ef4222521159880059a808f5a5a563af3789
Adam Sanderson <netghost@gmail.com>
2008-12-26 18:08:50 -0800

    updated game over screen.

--- game_over_screen.rb
+++ game_over_screen.rb
@@ -3,31 +3,24 @@ class GameOverScreen < AbstractScreen
     super()
     @title_font = Game.load_font 'Helvetica', 48
     
-    @score = Label.new(320, 256, "'#{round.initial_word}' #{round.score} words")
-    @words = round.words.split(',')
+    add @score = Label.new(320, 256, "'#{round.initial_word}' #{round.score} words")
+    add @words = round.words.split(',')
     
-    @new_game = Button.new(320, @score.bottom + 4, "New Game") do
+    add @new_game = Button.new(320, @score.bottom + 4, "New Game"){
       window.next_state = GameScreen.new()
-    end
+    }
     
-    @replay_game = Button.new(320, @new_game.bottom + 4, "Try Again") do
+    add @replay_game = Button.new(320, @new_game.bottom + 4, "Try Again"){
       window.next_state = GameScreen.new(round.initial_word)
-    end
+    }
   end
 
   def draw
     super
     @title_font.draw_rel("OckiDeux Game Over", Game.window.width/2, 10, Layers::UI, 0.5, 0, 1, 1, 0x666666FF, :default)
-    
-    @score.draw
-    @new_game.draw
-    @replay_game.draw
   end
   
   def update
-    @score.update
-    @new_game.update
-    @replay_game.update
   end
   
   def button_down(id)
@@ -35,10 +28,5 @@ class GameOverScreen < AbstractScreen
     when Gosu::KbEscape then  
       window.next_state = TitleScreen.new
     end
-  end
-  
-  def button_up(id)
-    @new_game.button_up(id)
-    @replay_game.button_up(id)
   end
 end
